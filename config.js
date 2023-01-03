589b1b48f9b8cbecbc3e3a259a000c2039aee1ac
Dendio-Playz <93475249+Dendio-Playz@users.noreply.github.com>
2023-01-03 12:27:49 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBJnzOMOs4LfUU_wzmRpNRdeuv2OHtS-Do",
+  authDomain: "story-telling-3ab7a.firebaseapp.com",
+  databaseURL: "https://story-telling-3ab7a-default-rtdb.firebaseio.com",
+  projectId: "story-telling-3ab7a",
+  storageBucket: "story-telling-3ab7a.appspot.com",
+  messagingSenderId: "530940596762",
+  appId: "1:530940596762:web:94fb8c96021ea9b8fbb7a3"
+};
